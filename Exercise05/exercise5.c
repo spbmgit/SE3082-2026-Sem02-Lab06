21597b91b551baf1b77f828fa74fc29a1ea17d19
spbmgit <it24103435@mysliit.lk>
2026-09-19 15:56:21 +0530

    Add Exercise 5: task parallelization of fibonacci

---
+++ Exercise05/exercise5.c
@@ -0,0 +1,35 @@
+#include <omp.h>
+#include <stdio.h>
+
+int fib(int n) {
+  int i, j;
+  if (n < 2)
+    return n;
+  else {
+    #pragma omp task shared(i)
+    i = fib(n-1);
+
+    #pragma omp task shared(j)
+    j = fib(n-2);
+
+    #pragma omp taskwait
+    return i + j;
+  }
+}
+
+int main() {
+  int n = 10;
+  int result;
+
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      result = fib(n);
+    }
+  }
+
+  printf("fib(%d) = %d\n", n, result);
+  return 0;
+}
+
